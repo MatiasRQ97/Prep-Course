3ee20dd13100d559bff51e59cc811e7353e7ae21
Matias Ronaldo Quiroga <ronaldo.m.j1997@gmail.com>
2022-06-30 08:32:39 -0300

    Hola

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,20 +28,22 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  suma = x + y;
+  return suma;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  resta = x - y;
+  return resta;
 }
 
 function multiplica(x, y) {
